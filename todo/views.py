147115d428dfeac8e3ccece5d8179036e3698317
LIKELION-YJOO <jym9809@sungkyul.ac.kr>
2023-04-04 13:58:19 +0900

    todo 모델 만들고 데이터 출력하기

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from todo.models import TodoList
 
 # Create your views here.
 def todos(request):
+	todolist = TodoList.objects.all()
 	return render(
 		request,
-		'todo/todos.html'
+		'todo/todos.html',
+  		{
+			'todolist': todolist,
+		}
+	)
+
+def index(request):
+	todolist = TodoList.objects.all()
+	return render(
+		request,
+		'todo/index.html',
+		{
+			'todolist': todolist,
+		}
 	)
